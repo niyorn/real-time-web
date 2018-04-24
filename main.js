e31a3838bd416c9748229a77914185325ca641c7
niyorn <c.niyornram@hotmail.com>
2018-04-24 19:20:45 +0200

    fixed connection error and add styling

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const router = require('./routes/routes');
 const port = 4000;
 const http = require('http')
 const server = http.createServer(app);
+const io = require('socket.io').listen(server);
 const connectURL = 'wss://streamer.cryptocompare.com';    
-const io = require('socket.io')(server)
 const cryptoSocket = require('socket.io-client')(connectURL);
-const subscription = ['0~Bitfinex~ETH~USD']
+const subscription = ['0~Binance~ETH~USDT']
 let initData = {}
 
 app
@@ -20,6 +20,7 @@ app.use('/', router)
 cryptoSocket.emit('SubAdd', { subs: subscription });
 cryptoSocket.on("m", function(data) {
     initData = data;
+    console.log(data)
     io.emit('data', data)
 });
 
--- public/script.js
+++ public/script.js
@@ -2,30 +2,113 @@
 
     const app = {
         init: function () {
-            crypto.init ();
+            crypto.socket;
             crypto.getData();
         }
     }
 
-
     const crypto = {
-        socket: socket = io (),
-        init: function () {
-            this.socket.on('init', function(data){
-                document.querySelector('h1').innerHTML = data
+        socket: socket = io(),
+        getData: function () {
+            let newPrice = 0;
+            this.socket.on('data', function(data){
+                crypto.transformData(data);
+
+                
+
+                // var incomingTrade = CCC.TRADE.unpack(data);
+                // let item = document.querySelector('#price');
+                // item.classList = "";
+                // let oldPrice = incomingTrade.P;
+        
+                // if(oldPrice > newPrice){
+                //     price.classList.add('gain');
+                //     newPrice = oldPrice;
+                // }
+                // else if(oldPrice < newPrice){
+                //     price.classList.add('lose');
+                //     newPrice = oldPrice;
+                // }
+
+                // item.innerHTML = newPrice;
+
+                // document.querySelector('iformation-container').appendChild(item)
+                
+                // document.body.appendChild(item);
             })
         },
-        getData: function () {
-            this.socket.on('data', function(data){
+        transformData : function (data) {
+            let incomingTrade = CCC.TRADE.unpack(data);
 
-                var incomingTrade = CCC.TRADE.unpack(data);
+            let trade = {
+                from : incomingTrade['FSYM'],
+                to : incomingTrade['TSYM'],
+                exchange : incomingTrade['M'],
+                quantity : incomingTrade['Q'],
+                price : incomingTrade['P'],
+                totalPrice : incomingTrade['TOTAL'],
+            }
 
-                console.table(incomingTrade)
-                let item = document.createElement('div')
-                item.appendChild(document.createTextNode(incomingTrade.P))
-                
-                document.body.appendChild(item);
-            })
+            let sell = 1,
+                buy = 2;
+
+            if(incomingTrade['F'] & sell) {
+                trade['type'] = 'SELL';
+            }
+            else if (incomingTrade['F'] & buy) {
+                trade['type'] = "BUY";
+            }
+            else {
+                trade['type'] = 'UNKNOWN';
+            }
+
+            this.displayData(trade)
+        },
+        displayData : function (data) {
+            let maxTableSize = 10;
+
+            let table = document.querySelector('.trade')
+            let length = table.rows.length;
+            let row = table.insertRow(0);
+
+            let exhange = document.createElement('td');
+            exhange.appendChild(document.createTextNode(data['exchange']));
+
+            let type = document.createElement('td');
+            type.appendChild(document.createTextNode(data['type']));
+
+            let from = document.createElement('td');
+            from.appendChild(document.createTextNode(data['from']));
+
+            let to = document.createElement('td');
+            to.appendChild(document.createTextNode(data['to']));
+
+            let price = document.createElement('td');
+            price.appendChild(document.createTextNode(data['price']));
+            price.classList.add('price')
+
+            let amount = document.createElement('td');
+            amount.appendChild(document.createTextNode(data['quantity']));
+
+            let total = document.createElement('td');
+            total.appendChild(document.createTextNode(Math.round((data['totalPrice']) *100 / 100)));
+            total.classList.add('price')
+
+            row.appendChild(exhange);
+            row.appendChild(type);
+            row.appendChild(from);
+            row.appendChild(to);
+            row.appendChild(price);
+            row.appendChild(amount);
+            row.appendChild(total);
+
+            if (length >= (maxTableSize)) {
+                table.deleteRow(10)
+            }
+
+            let title = document.querySelector('.title-price');
+            title.innerHTML = (data.price)
+
         }
     }
 
