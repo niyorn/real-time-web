5a1f677a418f6030a348758fd5ad4ba53604822b
niyorn <c.niyornram@hotmail.com>
2018-04-26 13:29:44 +0200

    put subscription in variable

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ const server = http.createServer(app);
 const io = require('socket.io').listen(server);
 const connectURL = 'wss://streamer.cryptocompare.com';    
 const cryptoSocket = require('socket.io-client')(connectURL);
-const subscription = ['0~Binance~ETH~USDT'];
+
+const subscriptionEth = '0~Binance~ETH~USDT';
+const subscriptionIcx = '0~Binance~ICX~ETH';
+const subscriptionNano = '0~Binance~NANO~ETH';
+const subscriptionReq = '0~Binance~REQ~ETH';
+const subscriptionAll = [subscriptionEth, subscriptionIcx, subscriptionNano, subscriptionReq];
 app
     .use(express.static('views'))
     .use(express.static('public'))
@@ -15,10 +20,18 @@ app
 
 app.use('/', router)
 
-cryptoSocket.emit('SubAdd', { subs: subscription });
+cryptoSocket.emit('SubAdd', { subs: subscriptionAll });
 cryptoSocket.on("m", function(data) {
+    console.log(data)
     io.emit('data', data)
 });
+
+io.on('connection', function (client){
+    
+    client.on('id', function (id){
+        console.log(id)
+    })
+})
 
 
 server.listen(port, function () {
